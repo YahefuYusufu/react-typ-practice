77041808f5d9f35d486c839423e7f948c8eb5060
prg0615 <parguzatt@gmail.com>
2022-11-08 20:15:23 +0100

    got type error

---
+++ todo-app/tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [
+    "./src/**/*.{js,jsx,ts,tsx,index.css}",
+    "./public/index.html",
+    "./src/index.css",
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+}
