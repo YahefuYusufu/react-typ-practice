b893c3d0a1d52205c36feda5af780bee3b7fde4d
prg0615 <parguzatt@gmail.com>
2022-11-15 21:19:34 +0100

    .

--- shopping-cart/src/components/StoreItem.tsx
+++ shopping-cart/src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
-import { Card } from "react-bootstrap"
+import { formatCurrency } from "../utilities/FormatCurrencey"
+import { Button, Card } from "react-bootstrap"
 
 interface ItemsProps {
   id: number
@@ -8,18 +9,46 @@ interface ItemsProps {
 }
 
 const StoreItem = ({ id, name, price, imgUrl }: ItemsProps) => {
+  const quantity = 0
   return (
-    <Card>
+    <Card className="h-100">
       <Card.Img
         variant="top"
         src={imgUrl}
         height="200px"
         style={{ objectFit: "cover" }}
-        className="rounded-3"
       />
-      <Card.Body>
-        <Card.Title>{name}</Card.Title>
-        <Card.Text>{price}</Card.Text>
+      <Card.Body className="d-flex flex-column">
+        <Card.Title className="d-flex justify-content-between">
+          <span className="fs-2">{name}</span>
+          <span className="ms-2 text-muted">{formatCurrency(price)}</span>
+        </Card.Title>
+        {/* buttons */}
+        <div className="m-auto">
+          {quantity === 0 ? (
+            <Button className="w-100">Add To Cart</Button>
+          ) : (
+            <div className="d-flex align-items-center flex-column">
+              <div
+                className="d-flex align-items-center justify-content-center"
+                style={{ margin: ".5rem" }}
+              >
+                <Button variant="outline-secondary" className="w-100">
+                  -
+                </Button>
+                <div>
+                  <span>{quantity}</span>in Cart
+                </div>
+                <Button variant="outline-secondary" className="w-100">
+                  +
+                </Button>
+              </div>
+              <Button variant="danger" size="sm">
+                Remove
+              </Button>
+            </div>
+          )}
+        </div>
       </Card.Body>
     </Card>
   )
