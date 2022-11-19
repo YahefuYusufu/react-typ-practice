86d86777aa52e3b468c7f4f23c272a3ad9106a7f
prg0615 <parguzatt@gmail.com>
2022-11-19 13:14:48 +0100

    redux

---
+++ redux-demo/index.js
@@ -0,0 +1,43 @@
+const redux = require("redux")
+const createStore = redux.createStore
+
+const CAKE_ORDERED = "CAKE_ORDERED"
+
+const orderCake = () => {
+  return {
+    type: "CAKE_ORDERED",
+    quantity: 1,
+  }
+}
+
+const initialState = {
+  numOfCakes: 10,
+}
+
+// (prevState,action) =>newState
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "CAKE_ORDERED":
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      }
+    default:
+      return state
+  }
+}
+
+const store = createStore(reducer)
+console.log("initialState", store.getState())
+
+const unsubscribe = store.subscribe(() =>
+  console.log("Update store", store.getState())
+)
+
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+
+unsubscribe()
