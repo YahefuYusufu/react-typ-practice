b6c70556d4ae4321748a6e312e26d4ff93f43ff0
prg0615 <parguzatt@gmail.com>
2022-12-07 06:21:58 +0100

    tailwind components utilities vatiriants

---
+++ Tailwind/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-full">
+      <button className="btn btn-gray">one</button>
+      <button className="btn btn-gray">two</button>
+      <button className="btn-blue hover:p-21">two</button>
+    </div>
+  )
+}
+
+export default App
