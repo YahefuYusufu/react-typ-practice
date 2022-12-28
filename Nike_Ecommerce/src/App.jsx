d75286b7f1936f83faa0a95c91808ac171326581
prg0615 <parguzatt@gmail.com>
2022-12-28 20:25:07 +0100

    footer & stories page had done

--- Nike_Ecommerce/src/App.jsx
+++ Nike_Ecommerce/src/App.jsx
@@ -1,15 +1,27 @@
 import React from "react"
-import { heroapi, popularsales, toprateslaes } from "./data/data.js"
-import { Hero, Sales } from "./components"
+import {
+  heroapi,
+  popularsales,
+  toprateslaes,
+  highlight,
+  sneaker,
+  story,
+  footerAPI,
+} from "./data/data.js"
+import { Hero, Sales, FlexContent, Stories, Footer } from "./components"
 
 const App = () => {
   return (
     <>
-      <main className="">
+      <main className="flex flex-col gap-16 relative">
         <Hero heroapi={heroapi} />
-        <Sales endpoint={popularsales} />
+        <Sales endpoint={popularsales} ifExists />
+        <FlexContent endpoint={highlight} ifExists />
+        <FlexContent endpoint={sneaker} />
         <Sales endpoint={toprateslaes} />
+        <Stories story={story} />
       </main>
+      <Footer footerAPI={footerAPI} />
     </>
   )
 }
--- Nike_Ecommerce/src/components/utilis/Item.jsx
+++ Nike_Ecommerce/src/components/utilis/Item.jsx
@@ -17,15 +17,22 @@ const Item = ({
   return (
     <>
       <div
-        className={`relative bg-gradient-to-b ${color}${shadow} grid items-center justify-items-center rounded-xl py-4 px-5 transition-all duration-700 ease-in-out w-full hover:scale-105`}
+        className={`relative bg-gradient-to-b ${color} ${shadow} grid items-center ${
+          ifExists ? "justify-items-start" : "justify-items-center"
+        } rounded-xl py-4 px-5 transition-all duration-700 ease-in-out w-full hover:scale-105`}
       >
-        <div className="grid items-center justify-items-center">
+        <div
+          className={`grid items-center ${
+            ifExists ? "justify-items-start" : "justify-items-center"
+          }`}
+        >
           <h1 className="text-slate-200 text-xl lg:text-lg md:text-base font-medium filter drop-shadow">
             {title}
           </h1>
           <p className="text-slate-200 filter drop-shadow text-base md:text-sm font-normal">
             {text}
           </p>
+
           <div className="flex items-center justify-between w-28 my-2">
             <div className="flex items-center bg-white/80  px-1 rounded blur-effect-theme">
               <h1 className="text-black text-sm font-medium">${price}</h1>
@@ -53,12 +60,19 @@ const Item = ({
             </button>
           </div>
         </div>
-
-        <div className="flex items-center">
+        <div
+          className={`flex items-center ${
+            ifExists ? "absolute top-5 right-1" : "justify-center"
+          }`}
+        >
           <img
             src={img}
-            alt="img/item/img"
-            className="transitions-theme hover:-rotate-12"
+            alt={`img/item-img/${id}`}
+            className={`transitions-theme hover:-rotate-12 ${
+              ifExists
+                ? "h-auto w-64 lg:w-56 md:w-48 -rotate-[35deg]"
+                : "h-36 w-64"
+            }`}
           />
         </div>
       </div>
