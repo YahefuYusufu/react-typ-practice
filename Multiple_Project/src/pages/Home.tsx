e5056eb4002ac8c694f54a68310a661b5dcfa3f9
prg0615 <parguzatt@gmail.com>
2023-01-13 16:49:21 +0100

    sa

--- Multiple_Project/src/pages/Home.tsx
+++ Multiple_Project/src/pages/Home.tsx
@@ -1,7 +1,12 @@
 const Home = () => {
   return (
     <>
-      <h1 className="flex justify-center  items-center h-screen">Home</h1>
+      <div className="flex justify-center items-center h-screen">
+        <div className="flex justify-center items-center flex-col  w-6/12 h-[350px] bg-gray-300 rounded-2xl">
+          <h1 className="text-4xl font-bold">Home</h1>
+          <p className="text-2xl">Welcome to the home page</p>
+        </div>
+      </div>
     </>
   )
 }
