b97d6d84469e9fadaad3378156ab7fb2eddfce3d
prg0615 <parguzatt@gmail.com>
2023-01-22 09:36:25 +0100

    in progress

--- tailwind-execize/src/components/Flex.jsx
+++ tailwind-execize/src/components/Flex.jsx
@@ -1,12 +1,18 @@
 import React from "react"
 const Flex = () => {
   return (
-    <div className="Parent">
-      <div>Header</div>
-      <div className="flex">
-        <div className="basis-4/12 text-center bg-slate-500">sidebar</div>
-        <div className="basis-6/12 text-center bg-green-500">Menu</div>
-        <div className="basis-4/12 text-center bg-yellow-500">Menu</div>
+    <div className="h-screen text-white bg-slate-900">
+      <div className="container mx-auto">
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 ">
+          <div className="p-6 rounded-lg bg-slate-800">First coloms</div>
+          <div className="p-6 rounded-lg bg-slate-700">Second coloms</div>
+          <div className="p-6 rounded-lg bg-slate-600">Third coloms</div>
+          <div className="p-6 col-span-2 rounded-lg bg-slate-500">
+            Fourth coloms
+          </div>
+          <div className="p-6 rounded-lg bg-slate-500">Fourth coloms</div>
+          <div className="p-6 rounded-lg bg-slate-500">Fourth coloms</div>
+        </div>
       </div>
     </div>
   )
